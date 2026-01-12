8aad670578cb6d261c4fb5cf10fabbed2e31fe2d
SulthanRaghib <raghib.smp@gmail.com>
2026-01-12 10:19:09 +0700

    add migrations for candidates and votes tables

---
+++ database/migrations/2026_01_12_023317_create_candidates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('vision')->nullable();
+            $table->text('mission')->nullable();
+            $table->string('photo_path')->nullable();
+            $table->integer('order_number')->default(0);
+            $table->bigInteger('votes_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
