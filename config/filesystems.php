6c17f8587aba70ac17095913941868f452492089
SulthanRaghib <raghib.smp@gmail.com>
2026-01-13 00:39:37 +0700

    Update public filesystem root path to point to storage directory

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('app/public'),
+            'root' => public_path('storage'),
             'url' => rtrim(env('APP_URL'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
