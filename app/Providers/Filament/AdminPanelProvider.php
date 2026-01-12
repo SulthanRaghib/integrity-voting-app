fb797bd53d13c5cc872a05e684cfb957e256ca2f
SulthanRaghib <raghib.smp@gmail.com>
2026-01-12 20:22:37 +0700

    Add ElectionStatusWidget and StatsOverviewWidget to Admin Panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,8 +46,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                \App\Filament\Widgets\ElectionStatusWidget::class,
+                \App\Filament\Widgets\StatsOverviewWidget::class,
+                \App\Filament\Widgets\LiveResultsChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
