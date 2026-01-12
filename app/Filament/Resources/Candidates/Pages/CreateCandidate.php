22a3fab7ea5f98ea83a608e3cf7d5d9e0f56b83b
SulthanRaghib <raghib.smp@gmail.com>
2026-01-12 18:15:12 +0700

    Add getRedirectUrl method to CreateCandidate and EditCandidate for consistent redirection

--- app/Filament/Resources/Candidates/Pages/CreateCandidate.php
+++ app/Filament/Resources/Candidates/Pages/CreateCandidate.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCandidate extends CreateRecord
 {
     protected static string $resource = CandidateResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/Candidates/Pages/EditCandidate.php
+++ app/Filament/Resources/Candidates/Pages/EditCandidate.php
@@ -16,4 +16,9 @@ class EditCandidate extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
